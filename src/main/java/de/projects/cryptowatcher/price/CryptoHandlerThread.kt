66364e8d282c0877e3ef23eacc4ff3b7b2334594
Andrei Precup <andreiprecup69@gmail.com>
2018-09-11 21:15:19 +0300

    Fix crash of application when there is no network connection.

--- src/main/java/de/projects/cryptowatcher/price/CryptoHandlerThread.kt
+++ src/main/java/de/projects/cryptowatcher/price/CryptoHandlerThread.kt
@@ -6,12 +6,18 @@ import de.projects.cryptowatcher.currencies.CryptoCurrencies
 import de.projects.cryptowatcher.currencies.FiatCurrencies
 import okhttp3.OkHttpClient
 import java.io.InputStreamReader
+import java.net.UnknownHostException
 
 class CryptoHandlerThread(private val url: String, private val crypto: CryptoCurrencies, private val fiat: FiatCurrencies, private val cryptoPriceService: CryptoPriceService) : HandlerThread("crypto") {
 	override fun run() {
 		val btcGetRequest = OkHttpClient.Builder().build().newCall(CryptoPriceService.createGetRequest(url))
-		Klaxon().pathMatcher(CurrencyMatcher(crypto, fiat, cryptoPriceService))
-				.pathMatcher(CurrencyPercentMatcher(crypto, cryptoPriceService))
-				.parseJsonObject(InputStreamReader(btcGetRequest!!.execute().body()!!.byteStream()))
+		try {
+			val response = btcGetRequest!!.execute()
+			Klaxon().pathMatcher(CurrencyMatcher(crypto, fiat, cryptoPriceService))
+					.pathMatcher(CurrencyPercentMatcher(crypto, cryptoPriceService))
+					.parseJsonObject(InputStreamReader(response.body()!!.byteStream()))
+		} catch (e : UnknownHostException) {
+			quit()
+		}
 	}
 }
