6fc2ef9c77014cc644c99b2596e9e37652ca1829
Andrei Precup <andreiprecup69@gmail.com>
2024-04-20 21:56:27 +0300

    Update CoinDesk endpoints

--- src/main/java/de/projects/cryptowatcher/price/CryptoHandlerThread.kt
+++ src/main/java/de/projects/cryptowatcher/price/CryptoHandlerThread.kt
@@ -3,21 +3,21 @@ package de.projects.cryptowatcher.price
 import android.os.HandlerThread
 import com.beust.klaxon.Klaxon
 import de.projects.cryptowatcher.currencies.CryptoCurrencies
-import de.projects.cryptowatcher.currencies.FiatCurrencies
 import okhttp3.OkHttpClient
 import java.io.InputStreamReader
 import java.net.SocketTimeoutException
 import java.net.UnknownHostException
 
-class CryptoHandlerThread(private val url: String, private val crypto: CryptoCurrencies, private val fiat: FiatCurrencies, private val cryptoPriceService: CryptoPriceService) : HandlerThread("crypto") {
+class CryptoHandlerThread(private val url: String, private val crypto: CryptoCurrencies, private val cryptoPriceService: CryptoPriceService) : HandlerThread("crypto") {
 	override fun run() {
-		val btcGetRequest = OkHttpClient.Builder().build().newCall(CryptoPriceService.createGetRequest(url))
+		val cryptoGetRequest = OkHttpClient.Builder().build().newCall(CryptoPriceService.createGetRequest(url))
 		try {
-			val response = btcGetRequest!!.execute()
-			Klaxon().pathMatcher(CurrencyMatcher(crypto, fiat, cryptoPriceService))
-					.pathMatcher(CurrencyPercentMatcher(crypto, fiat, cryptoPriceService))
+			val response = cryptoGetRequest!!.execute()
+			Klaxon().pathMatcher(CurrencyMatcher(crypto, cryptoPriceService))
+					.pathMatcher(CurrencyPercentMatcher(crypto, cryptoPriceService))
 					.parseJsonObject(InputStreamReader(response.body()!!.byteStream()))
 		} catch (e: UnknownHostException) {
+			cryptoPriceService.broadcastNetworkError()
 		} catch (e: SocketTimeoutException) {
 			cryptoPriceService.broadcastNetworkError()
 		}
--- src/main/java/de/projects/cryptowatcher/price/CurrencyPercentMatcher.kt
+++ src/main/java/de/projects/cryptowatcher/price/CurrencyPercentMatcher.kt
@@ -3,13 +3,11 @@ package de.projects.cryptowatcher.price
 import android.content.Intent
 import com.beust.klaxon.PathMatcher
 import de.projects.cryptowatcher.currencies.CryptoCurrencies
-import de.projects.cryptowatcher.currencies.FiatCurrencies
 import de.projects.cryptowatcher.intents.CryptoIntents
 import java.util.regex.Pattern
 
-class CurrencyPercentMatcher(private val crypto: CryptoCurrencies, private val currencyFiat: FiatCurrencies, private val cryptoPriceService: CryptoPriceService) : PathMatcher {
-	override fun pathMatches(path: String): Boolean = Pattern.matches(".*data.*currency.*$crypto.*$currencyFiat.*percent.*", path)
-
+class CurrencyPercentMatcher(private val crypto: CryptoCurrencies, private val cryptoPriceService: CryptoPriceService) : PathMatcher {
+	override fun pathMatches(path: String): Boolean = Pattern.matches(".*data.*$crypto.*change.*percent.*", path)
 	override fun onMatch(path: String, value: Any) {
 		val intent = Intent("${CryptoIntents.ACTION_CRYPTO_PERCENT_LOADED}")
 		intent.putExtra("$crypto PERCENT", value.toString())
